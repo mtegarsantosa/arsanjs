cce34a8296e18edf9038acc56b9e3d7371481a70
Arsan Dev <tganteng@gmail.com>
2019-09-11 14:27:39 +0700

    title fixing

--- app/controllers/AController.js
+++ app/controllers/AController.js
@@ -1,6 +1,8 @@
 // const MyModel = require('../models/MyModel')
 module.exports = {
  welcome(req,res){
-   res.render('guest/welcome')
+   res.render('guest/welcome',{
+     title:'Welcome to Arsan JS'
+   })
   }
 }
